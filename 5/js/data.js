176608f3354ddf58320247aa4d97570da3ca7268
peachymissy <835276912@qq.com>
2018-10-30 14:15:06 +0800

    1
    1

---
+++ 5/js/data.js
@@ -0,0 +1,38 @@
+$("button#get_data").click(function() {
+       var items = [];
+       var i = 0;
+       var airtable_read_endpoint = "https://api.airtable.com/v0/appLS27z0VKDfU7Ca/SUBSTITUTE?maxRecords=3&view=Grid%20view";
+       var dataSet = [];
+       $.getJSON(airtable_read_endpoint, function(result) {
+              $.each(result.records, function(key,value) {
+                  items = [];
+                      items.push(value.fields.Name);
+                      items.push(value.fields.Similar_MAC_lipsticks);
+                      items.push(value.fields.brand);
+                      items.push(value.fields.Substitutes_score);
+                      items.push(value.fields.MAC_score);
+                      items.push(value.fields.price);
+                      dataSet.push(items);
+               }); // end .each
+            $('#example').DataTable( {
+                data: dataSet,
+                retrieve: true,
+                columns: [
+                    { title: "Name",
+                      defaultContent:""},
+                    { title: "SIMILAR MAC LIPSTICK",
+                      defaultContent:"" },
+                    { title: "brand",
+                      defaultContent:""},
+                      { title: "Substitutes_score",
+                        defaultContent:""},
+                        { title: "MAC_score",
+                            defaultContent:""},
+                      { title: "price",
+                    defaultContent:""},
+
+                ]
+            } );
+       }); // end .getJSON
+    }); // end button
+}); // document ready
