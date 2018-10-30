f79d4af136fb093ee0b479b2e84e24c196f811ff
peachymissy <835276912@qq.com>
2018-10-30 15:12:27 +0800

    1
    1

--- 5/data.js
+++ 5/data.js
@@ -1,7 +1,8 @@
 $("button#get_data").click(function() {
        var items = [];
        var i = 0;
-       var airtable_read_endpoint = "https://api.airtable.com/v0/appLS27z0VKDfU7Ca/SUBSTITUTE?maxRecords=3&view=Grid%20view";
+       var airtable_read_endpoint = "https://api.airtable.com/v0/appLS27z0VKDfU7Ca/SUBSTITUTE?maxRecords=3&view=Grid%20view" \
+-H "Authorization: Bearer keypu3fH9W07tffvh";
        var dataSet = [];
        $.getJSON(airtable_read_endpoint, function(result) {
               $.each(result.records, function(key,value) {
@@ -33,6 +34,3 @@ $("button#get_data").click(function() {
 
                 ]
             } );
-       }); // end .getJSON
-    }); // end button
-}); // document ready
